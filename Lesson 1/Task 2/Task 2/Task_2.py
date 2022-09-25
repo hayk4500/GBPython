16e43699d0e5b189e7bb64505a3e1842a362e644
hayk4500 <108433596+hayk4500@users.noreply.github.com>
2022-09-26 00:28:38 +0300

    Add task 2

--- Lesson 1/Task 2/Task 2/Task_2.py
+++ Lesson 1/Task 2/Task 2/Task_2.py
@@ -1,1 +1,11 @@
+X = int(input("X = "))
+Y = int(input("Y = "))
+Z = int(input("Z = "))
 
+statement1 = not (X or Y or Z)
+statement2 = not (X) and not (Y) and not (Z)
+
+if statement1 == statement2:
+    print("true")
+else:
+    print("false")
